67d2b1d8c762a908c92ed8907668094c01b8a4fb
Jiyu <jiyu01160@naver.com>
2025-04-01 16:29:24 +0900

    ~46번 강의 (CSS 적용 안 된 부분 수정 필요)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import requests from "./api/requests";
 import './App.css';
 import Nav from "./components/Nav";
+import Banner from './components/Banner';
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
       <Nav></Nav>
-      <Banner />
+      <Banner></Banner>
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
+
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      ></Row>
+      
+      <Row
+        title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      ></Row>
+
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      ></Row>
+
+      <Row
+        title="Comedy Movies"
+        id="CM"
+        fetchUrl={requests.fetchComedyMovies}
+      ></Row>
     </div>
   );
 };
--- src/api/requests.js
+++ src/api/requests.js
@@ -1,5 +1,5 @@
 const requests = {
-    fetchNowPlaying: "movie/now_playing",
+    fetchNowPlaying: "/movie/now_playing",
     fetchNetflixOriginals: "/discover/tv?with_networks=213",
     fetchTrending: "/trending/all/week",
     fetchTopRated: "/movie/top_rated",
@@ -7,7 +7,7 @@ const requests = {
     fetchComedyMovies: "/discover/movie?with_genres=35",
     fetchHorrorMovies: "/discover/movie?with_genres=27",
     fetchRomanceMovies: "/discover/movie?with_genres=10749",
-    fetchDocumnetaries: "/discover/movie?with_genres=99",
+    fetchDocumentaries: "/discover/movie?with_genres=99",
 };
 
 export default requests;
